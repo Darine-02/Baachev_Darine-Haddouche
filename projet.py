71b41ec25c867deff6a5e031412362655a5a4eae
Darine-02 <milarine2002@gmail.com>
2025-12-11 23:28:00 +0000

    Réponse 1 (Création et affichage du DataFrame)

---
+++ projet.py
@@ -0,0 +1,26 @@
+#Haddouche Darine Aya , BAACHEV , 13/12/2025
+#Semahi Ibtisam
+#Lakoues Feryal
+#Bouklikha Ines
+
+
+import pandas as pd 
+import numpy as np
+
+#Données : Séquence ADN , Longueur , Pourcentage GC
+data = { 
+    "Séquence":["ATGCGTACGTA", "GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGC", "TTAACCGGAT"],
+    "Longueur":[12, 12, 12, 10, 11, 10, 10],
+    "Pourcentage GC":[50, 66.67, 58.33, 40, 45.45, 60, 50]
+}
+
+    
+# 1) Créer et afficher le tableau en utilisant pandas 
+print("*********** Création et affichage du DataFrame **************")
+df = pd.DataFrame(data)
+
+
+#Affichage Du Tableau 
+print ("Tableau De Séquence ADN :")
+print (df)
+print ("-" * 70)
